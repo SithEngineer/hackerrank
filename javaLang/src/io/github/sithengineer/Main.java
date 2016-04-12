f022dbb8a4485dd17e8b7fffa62cfd826a3ce750
Fábio Dias <fabio.a.s.dias@gmail.com>
2016-04-12 16:59:53 +0100

    one more challenge complete in the "collection" section of "java" language

--- javaLang/src/io/github/sithengineer/Main.java
+++
@@ -1,6 +0,0 @@
-package io.github.sithengineer;
-
-public class Main {
-
-    public static void main(String[] args) { }
-}
---
+++ javaLang/src/io/github/sithengineer/java/collections/oneD_array.java
@@ -0,0 +1,28 @@
+package io.github.sithengineer.java.collections;
+
+import java.util.Scanner;
+
+public class oneD_array {
+
+    static boolean canWinTheGame(int[] arr, int m) {
+        boolean endIsReachable = false;
+
+        // TODO
+
+        return endIsReachable;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = in.nextInt();
+        for(int testCase = 0; testCase<testCases; ++testCase) {
+            int n = in.nextInt();
+            int m = in.nextInt();
+            int[] arr = new int [n];
+            for(int i=0 ; i<n ; ++i) {
+                arr[i] = in.nextInt();
+            }
+            System.out.println( canWinTheGame(arr, m) ? "YES" : "NO");
+        }
+    }
+}
