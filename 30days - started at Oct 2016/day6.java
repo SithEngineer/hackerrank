11e664819bb58403ee1fb0f89077970bfafb741b
Fábio Dias <fabio.a.s.dias@gmail.com>
2017-01-05 00:59:05 +0000

    added changes and more 30 days challenge solutions

---
+++ 30days - started at Oct 2016/day6.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // could be done with just one array, but has more complexity to read
+        StringBuilder pairs = new StringBuilder();
+        StringBuilder impairs = new StringBuilder();
+        String data = null;
+
+        int tests = Integer.parseInt(scan.nextLine());
+        for(int testCase = 0 ; testCase < tests ; ++testCase) {  
+            data = scan.nextLine();
+            for(int idx = 0; idx < data.length() ; ++idx) {
+                if(idx%2==0){
+                    pairs.append(data.charAt(idx));
+                } else {
+                    impairs.append(data.charAt(idx));                    
+                }                
+            }        
+            System.out.printf("%s %s\n", pairs.toString(), impairs.toString());
+            pairs.delete(0, pairs.length());
+            impairs.delete(0, impairs.length());
+        }
+        scan.close();
+    }
+}
