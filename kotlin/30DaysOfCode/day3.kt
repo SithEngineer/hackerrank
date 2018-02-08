95d20f47fa2d761ecbf73237f9f4ad848e71d5b8
Fábio Dias <sithengineer@protonmailcom>
2018-02-08 13:33:15 +0000

    minor kotlin code to test platform kotlin support

---
+++ kotlin/30DaysOfCode/day3.kt
@@ -0,0 +1,18 @@
+// If n is odd, print Weird
+// If n is even and in the inclusive range of 2 to 5, print Not Weird
+// If n is even and in the inclusive range of 6 to 20, print Weird
+// If n is even and greater than 20, print Not Weird
+
+import java.io.*
+import java.util.*
+
+fun main(args: Array<String>) {
+	val input = Scanner(System.`in`)
+	val n = input.nextInt()
+	input.close()
+    if(n%2!=0 || (n%2==0 && n>=6 && n<=20)){
+    	println("Weird")
+    }else{
+    	println("Not Weird")
+    }
+}
